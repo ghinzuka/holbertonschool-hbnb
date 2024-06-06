d46ab0d413fc42d4bcb0440ad460fa88289160ac
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-06 10:30:38 +0200

    creating a new class called User with innit method to initialize attributes

--- models/user.py
+++ models/user.py
@@ -1,3 +1,19 @@
+import re
+from typing import List
+from uuid import UUID, uuid4
+from datetime import datetime
+from place import Place
+from review import Review
+
+
 class User:
-    """Define a User"""
-    def __init__(self, email, password, first_name, last_name, ):
+    def __init__(self, email: str, password: str, first_name: str, last_name: str):
+        self.user_id = uuid4()
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.places = []
+        self.reviews = []
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
