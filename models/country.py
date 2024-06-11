c0b352a1d44394b770d572fc1204c1dce2552b43
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-11 10:45:12 +0200

    fixing import of city

--- models/country.py
+++ models/country.py
@@ -1,7 +1,8 @@
 from .base import BaseModel
+from .city import City
 
 class Country(BaseModel):
-    def __init__(self, city, name: str):
+    def __init__(self, city: City, name: str):
         super().__init__()
         self.name = name
         self.city = city
@@ -12,6 +13,16 @@ class Country(BaseModel):
 
     @name.setter
     def name(self, value):
-        if not isinstance(value, str):
-            raise TypeError("name must be a string")
+        if not isinstance(value, str) or not value:
+            raise TypeError("name must be a non-empty string")
         self.__name = value
+    
+    @property
+    def city(self):
+        return self.__city
+    
+    @city.setter
+    def city(self, value):
+        if not isinstance(value, City):
+            raise TypeError("city must be a City instance")
+        self.__city = value
