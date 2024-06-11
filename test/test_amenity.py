3a352fe9f7de12d0ca4d5ce9796c7b225692c47d
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-11 13:49:49 +0200

    changfin test file

--- test/test_amenity.py
+++ test/test_amenity.py
@@ -2,48 +2,39 @@ import unittest
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from persistence.datamanager import DataManager
 from models.amenity import Amenities
+from persistence.datamanager import DataManager
 
 class TestAmenity(unittest.TestCase):
     def setUp(self):
-        self.datamanager = DataManager(MockStorage())
+        # Créer une instance de DataManager avec un chemin de fichier spécifique pour le stockage des données JSON
+        self.file_path = "test_amenities.json"  # Chemin de fichier pour stocker les données JSON
+        self.datamanager = DataManager(self.file_path)
+
+    def tearDown(self):
+        # Supprimer le fichier JSON après chaque test
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
 
     def test_amenity_crud(self):
         # Création d'un équipement
-        amenity = Amenities("Pool")
+        amenity = Amenities('Pool')
         self.datamanager.create(amenity)
 
         # Récupération de l'équipement
         retrieved_amenity = self.datamanager.read(amenity.id, Amenities)
-        self.assertEqual(retrieved_amenity.name, "Pool")
+        self.assertEqual(retrieved_amenity.name, 'Pool')
 
         # Mise à jour de l'équipement
-        amenity.name = "Updated Amenity"
+        amenity.name = 'Updated Pool'
         self.datamanager.update(amenity)
         updated_amenity = self.datamanager.read(amenity.id, Amenities)
-        self.assertEqual(updated_amenity.name, "Updated Amenity")
+        self.assertEqual(updated_amenity.name, 'Updated Pool')
 
         # Suppression de l'équipement
         self.datamanager.delete(amenity.id, Amenities)
         deleted_amenity = self.datamanager.read(amenity.id, Amenities)
         self.assertIsNone(deleted_amenity)
-
-class MockStorage:
-    def __init__(self):
-        self.data = {}
-
-    def create(self, entity):
-        self.data[(entity.id, type(entity))] = entity
-
-    def read(self, entity_id, entity_type):
-        return self.data.get((entity_id, entity_type))
-
-    def update(self, entity):
-        self.data[(entity.id, type(entity))] = entity
-
-    def delete(self, entity_id, entity_type):
-        del self.data[(entity_id, entity_type)]
 
 if __name__ == '__main__':
     unittest.main()
