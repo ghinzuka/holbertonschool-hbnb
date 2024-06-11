7a564935ea4c007c32050c6fb159a1e1db52bd58
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-11 09:28:34 +0200

    creation user with BM

--- models/user.py
+++ models/user.py
@@ -1,82 +1,55 @@
-from uuid import UUID, uuid4
-from datetime import datetime
-from review import Review
-from place import Place
+from base import BaseModel
 
-class User:
-    _registered_emails = set()
-    _users_data = []
 
-    def __init__(self, email: str, password: str, first_name: str, last_name: str):
-        if email in User._registered_emails:
-            raise ValueError(f"Email {email} is already taken.")
-        self.user_id = uuid4()
+class User(BaseModel):
+    def __init__(
+            self,
+            email: str,
+            password: str,
+            first_name: str,
+            last_name: str):
+        super().__init__()
         self.email = email
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
-        self.place_names = []  # Placeholder for place names
-        self.reviews = []  
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        User._registered_emails.add(email)
-        
-        # Add user data to the list of dictionaries
-        User._users_data.append({
-            'user_id': self.user_id,
-            'email': self.email,
-            'password': self.password,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'place_names': self.place_names,
-            'reviews': self.reviews,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at
-        })
 
-    def update(self, **kwargs):
-        for key, value in kwargs.items():
-            if hasattr(self, key):
-                if key == "email":
-                    if value in User._registered_emails:
-                        raise ValueError(f"Email {value} is already taken.")
-                    # Update the email set
-                    User._registered_emails.remove(self.email)
-                    User._registered_emails.add(value)
-                setattr(self, key, value)
-        self.updated_at = datetime.now()
+    @property
+    def email(self):
+        return self._email
 
-    def load_places_names(self):
-        self.place_names = []
-        for place_data in Place.get_places():
-            if place_data['user_id'] == self.user_id:
-                place_name = place_data['name']
-                self.place_names.append(place_name)
+        @email.setter
+        def email(self, value):
+            if not isinstance(value, str) or not value:
+                raise TypeError("email must be a non-empty string")
+            self._email = value
 
-    def load_reviews(self):
-        self.reviews = []
-        for review_data in Review.get_reviews():
-            if review_data['user_id'] == self.user_id:
-                review = Review(**review_data)
-                self.reviews.append(review)
+        @property
+        def password(self):
+            return self._password
 
-    @classmethod
-    def remove_user(cls, user):
-        if user.email in cls._registered_emails:
-            cls._registered_emails.remove(user.email)
-            # Remove user data from the list
-            cls._users_data = [data for data in cls._users_data if data['email'] != user.email]
+        @password.setter
+        def password(self, value):
+            if not isinstance(value, str) or not value:
+                raise TypeError("password must be a non-empty string")
+            self._password = value
 
-    @classmethod
-    def find_user_by_email(cls, email):
-        for user_data in cls._users_data:
-            if user_data['email'] == email:
-                return cls(**user_data)
-        return None
+        @property
+        def first_name(self):
+            return self._first_name
 
-    @classmethod
-    def find_user_by_id(cls, user_id):
-        for user_data in cls._users_data:
-            if user_data['user_id'] == user_id:
-                return cls(**user_data)
-        return None
+        @first_name.setter
+        def first_name(self, value):
+            if not isinstance(value, str) or not value:
+                raise TypeError("first_name must be a non-empty string")
+            self._first_name = value
+
+        @property
+        def last_name(self):
+            return self._last_name
+
+        @last_name.setter
+        def last_name(self, value):
+            if not isinstance(value, str) or not value:
+                raise TypeError("last_name must be a non-empty string")
+            self._last_name = value
