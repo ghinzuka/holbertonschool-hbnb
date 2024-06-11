af7a8bf2dc568a42282a8bce68d6599824c54513
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-11 09:58:28 +0200

    fix pycodestyle

--- models/user.py
+++ models/user.py
@@ -17,38 +17,38 @@ class User(BaseModel):
     def email(self):
         return self._email
 
-        @email.setter
-        def email(self, value):
-            if not isinstance(value, str) or not value:
-                raise TypeError("email must be a non-empty string")
-            self._email = value
+    @email.setter
+    def email(self, value):
+        if not isinstance(value, str) or not value:
+            raise TypeError("email must be a non-empty string")
+        self._email = value
 
-        @property
-        def password(self):
-            return self._password
+    @property
+    def password(self):
+        return self._password
 
-        @password.setter
-        def password(self, value):
-            if not isinstance(value, str) or not value:
-                raise TypeError("password must be a non-empty string")
-            self._password = value
+    @password.setter
+    def password(self, value):
+        if not isinstance(value, str) or not value:
+            raise TypeError("password must be a non-empty string")
+        self._password = value
 
-        @property
-        def first_name(self):
-            return self._first_name
+    @property
+    def first_name(self):
+        return self._first_name
 
-        @first_name.setter
-        def first_name(self, value):
-            if not isinstance(value, str) or not value:
-                raise TypeError("first_name must be a non-empty string")
-            self._first_name = value
+    @first_name.setter
+    def first_name(self, value):
+        if not isinstance(value, str) or not value:
+            raise TypeError("first_name must be a non-empty string")
+        self._first_name = value
 
-        @property
-        def last_name(self):
-            return self._last_name
+    @property
+    def last_name(self):
+        return self._last_name
 
-        @last_name.setter
-        def last_name(self, value):
-            if not isinstance(value, str) or not value:
-                raise TypeError("last_name must be a non-empty string")
-            self._last_name = value
+    @last_name.setter
+    def last_name(self, value):
+        if not isinstance(value, str) or not value:
+            raise TypeError("last_name must be a non-empty string")
+        self._last_name = value
