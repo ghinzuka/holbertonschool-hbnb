147f9f2b0eab781b5f9b484f0ee34776a94bd1bf
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-11 09:27:53 +0200

    creation of country with base model

--- models/country.py
+++ models/country.py
@@ -1,25 +1,17 @@
-from typing import List
-from city import City
+from base import BaseModel
 
-class Country:
-    def __init__(self, name: str, cities: List[City] = None):
+class Country(BaseModel):
+    def __init__(self, city, name: str):
+        super().__init__()
         self.name = name
-        self.cities = cities or []
-    
+        self.city = city
+
     @property
     def name(self):
         return self.__name
-    
+
     @name.setter
     def name(self, value):
         if not isinstance(value, str):
             raise TypeError("name must be a string")
         self.__name = value
-    
-    def add_city(self, city: List[City]):
-        if city in self.cities:
-            raise ValueError(f"City {city.name} is already added to this country")
-        self.cities.append(city)
-
-    def get_all_cities(self):
-        return list(City.get_all_cities())
