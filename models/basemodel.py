3267a905dd0ba28ebbe729b08fe7bb7c00ae88c5
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-11 13:51:56 +0200

    removing basemodel file

--- models/basemodel.py
+++
@@ -1,8 +0,0 @@
-from uuid import uuid4
-from datetime import datetime
-
-class BaseModel:
-    def __init__(self):
-        self.id = uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
