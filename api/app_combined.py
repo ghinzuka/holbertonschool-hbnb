462f360a183cfe4afad8e3a678fd8bc2c144ea5f
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-12 23:14:22 +0200

    Yeah solve the problem of multiple root

--- api/app_combined.py
+++ api/app_combined.py
@@ -4,7 +4,6 @@ from api.app_amenity import amenity_bp  # Importez le Blueprint amenity_bp
 
 app = Flask(__name__)
 
-# Enregistrez les Blueprints pour les API utilisateur et amenity
-app.register_blueprint(amenity_bp)
-app.register_blueprint(user_bp)  # Enregistrez le Blueprint user_bp
-  # Enregistrez le Blueprint amenity_bp
+# Enregistrez les Blueprints pour les API utilisateur et amenity avec des préfixes de chemin
+app.register_blueprint(user_bp, url_prefix='/users')
+app.register_blueprint(amenity_bp, url_prefix='/amenities')
